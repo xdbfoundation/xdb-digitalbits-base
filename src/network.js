604dd44169985efb8a6a30c0d11e41ba8a551e42
alex-tabachuk <a.tabachuk@geniusee.com>
2023-12-06 19:22:04 +0200

    new livenet passphrases

--- src/network.js
+++ src/network.js
@@ -5,7 +5,7 @@
  * @type {{PUBLIC: string, TESTNET: string}}
  */
 export const Networks = {
-  PUBLIC: 'LiveNet Global DigitalBits Network ; February 2021',
-  TESTNET: 'TestNet Global DigitalBits Network ; December 2020',
+  PUBLIC: 'LiveNet Global XDBChain Network ; November 2023',
+  TESTNET: 'Futurenet XDBChain Network ; October 2023',
   DEVNET: 'LiveNet Global DigitalBits Network ; February 2021'
 };
